e6b72734136e0239f1e585e76886ce75cd61638a
Usman Ubaid <usmanubaid9@gmail.com>
2026-09-12 23:22:52 +0200

    feat: add core models for companies, jobs and applications

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -44,7 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users'
+    'users',
+    'jobs',
+    'companies',
+    'applications'
 ]
 
 MIDDLEWARE = [
